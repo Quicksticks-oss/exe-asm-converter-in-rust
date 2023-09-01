f9c40c487d84c738359fb2f726aa86083fe42d73
Quicksticks-oss <chongobongotoday@gmail.com>
2023-09-01 19:53:02 -0400

    First

--- converter/src/main.rs
+++ converter/src/main.rs
@@ -1,3 +1,56 @@
+extern crate capstone;
+use capstone::prelude::*;
+use std::fs;
+
+/// Print register names
+fn reg_names(cs: &Capstone, regs: &[RegId]) -> String {
+    let names: Vec<String> = regs.iter().map(|&x| cs.reg_name(x).unwrap()).collect();
+    names.join(", ")
+}
+
+/// Print instruction group names
+fn group_names(cs: &Capstone, regs: &[InsnGroupId]) -> String {
+    let names: Vec<String> = regs.iter().map(|&x| cs.group_name(x).unwrap()).collect();
+    names.join(", ")
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Read the file's contents into a byte array
+    let file_contents = fs::read("/media/reaktor/Data Drive/GithubRepos/exe-asm-converter-in-rust/converter/wordpad.exe").expect("Failed to read file");
+
+    let cs = Capstone::new()
+        .x86()
+        .mode(arch::x86::ArchMode::Mode64)
+        .syntax(arch::x86::ArchSyntax::Att)
+        .detail(true)
+        .build()
+        .expect("Failed to create Capstone object");
+
+    let insns = cs.disasm_all(&file_contents, 0x1000).expect("Failed to disassemble");
+    println!("Found {} instructions", insns.len());
+    for i in insns.as_ref() {
+        println!();
+        println!("{}", i);
+
+        let detail: InsnDetail = cs.insn_detail(&i).expect("Failed to get insn detail");
+        let arch_detail: ArchDetail = detail.arch_detail();
+        let ops = arch_detail.operands();
+
+        let output: &[(&str, String)] = &[
+            ("insn id:", format!("{:?}", i.id().0)),
+            ("bytes:", format!("{:?}", i.bytes())),
+            ("read regs:", reg_names(&cs, detail.regs_read())),
+            ("write regs:", reg_names(&cs, detail.regs_write())),
+            ("insn groups:", group_names(&cs, detail.groups())),
+        ];
+
+        for &(ref name, ref message) in output.iter() {
+            println!("{:4}{:12} {}", "", name, message);
+        }
+
+        println!("{:4}operands: {}", "", ops.len());
+        for op in ops {
+            println!("{:8}{:?}", "", op);
+        }
+    }
 }
